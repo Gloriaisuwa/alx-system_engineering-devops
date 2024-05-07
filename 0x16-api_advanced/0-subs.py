0fcbdb19db1f3b69687981ecbee7515a4dba3c04
Gloriaisuwa <gloriaisuwa@gmail.com>
2024-05-07 11:55:23 -0700

    updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,21 +8,20 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Retrieves the number of subscribers for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        int: The number of subscribers of the subreddit. Returns 0 if the
-             subreddit is invalid or not found.
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Custom User Agent'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json()
-        return data.get('data').get('subscribers')
-    else:
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
         return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,17 +12,20 @@ def top_ten(subreddit):
 
     Args:
         subreddit (str): The name of the subreddit.
-
-    Returns:
-        None
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'MyRedditBot/1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Custom User Agent'}
+    params = {'limit': 10}
 
+    response = requests.get(url, headers=headers, params=params)
     if response.status_code == 200:
-        data = response.json().get('data').get('children')
-        for post in data:
-            print(post['data']['title'])
+        data = response.json().get('data', {})
+        children = data.get('children', [])
+        if children:
+            for i, child in enumerate(children, 1):
+                if not child['data']['stickied']:  # Skip stickied posts
+                    print(f"{i}. {child['data']['title']}")
+        else:
+            print("No hot posts found for this subreddit.")
     else:
         print("None")
